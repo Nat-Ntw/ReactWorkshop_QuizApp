0d8868cfc1a9cf63d349097ec35f5cd51a42469d
Nat-Ntw <nattawong.sir@gmail.com>
2022-11-23 16:16:13 +0700

    upload

---
+++ src/components/Menu.js
@@ -0,0 +1,14 @@
+import { useContext } from "react"
+import { DataContext } from "../App"
+
+const Menu = ()=>{
+    const {setAppState} = useContext(DataContext)
+    return(
+        <div className="menu">
+            <h1>แบบทดสอบก่อนเรียน😶‍🌫️</h1>
+            <button onClick={()=>setAppState("quiz")}>เริ่มทำแบบทดสอบ</button>
+        </div>
+    )
+}
+
+export default Menu
